931e293e182f3c68c073c65226560f28ee016c7d
Ashok Timilsina <ashok@timilsina.com>
2022-06-11 16:43:32 -0400

    revised js and css file

--- script.js
+++ script.js
@@ -2,61 +2,44 @@ const memepage = document.querySelector(".memeform");
 const imageInput = document.querySelector("#url");
 const topTxt = document.querySelector("#text1");
 const bottomTxt = document.querySelector("#text2");
-
-let div1 = document.querySelector(' .mydiv');
-
- 
-
-let h = document.createElement("span")
-let b =document.createElement("span")
+let div1 = document.querySelector('.mydiv'); 
 
 memepage.addEventListener("submit", function(e){
     e.preventDefault();
-        
-    let key = imageInput.value;    
-    reqImg(key); 
-         
-    let val = topTxt.value;
-    addTopText(val);
-    h.setAttribute('class','top');
+    let div = document.createElement('div');
+    div1.appendChild(div)
+    div.setAttribute('class', 'newDiv');
 
-    let message = bottomTxt.value;
-    addBottomText(message);
-    b.setAttribute('class','bottom')
-    
+    let key = imageInput.value;
+    let val = topTxt.value;
+    let message = bottomTxt.value; 
+
+
+    reqImg(key,val,message, div);
+    div.addEventListener('click', function(e){
+        e.preventDefault();
+        let touch = e.target.parentElement;
+        touch.remove();
+})
+});
+function reqImg (keys,text,note, div){   
     imageInput.value = '';
     topTxt.value = '';
-    bottomTxt.value = '';
-    
-
-})
-
-
-function reqImg (keys){ 
-   
-    let image = document.createElement("img");     
+    bottomTxt.value = ''; 
+    let image = document.createElement("img");
+    image.classList.add("meme")    
     image.setAttribute("src", keys)       
-    div1.appendChild(image)  
-    image.setAttribute('class','meme')    
-}   
-function addTopText(text){
-    let newtext = document.createTextNode(text);
-    div1.appendChild(h)    
-    h.appendChild(newtext)
+    div.appendChild(image) 
 
+    let header_text = document.createElement('h2')
+        header_text.classList.add("top")
+        header_text.innerText = text
+        div.appendChild(header_text)
+
+    let footer_text = document.createElement('h2')
+        footer_text.classList.add("bottom")
+        footer_text.innerText = note
+        div.appendChild(footer_text);    
+  
+    return reqImg;
 }
-function addBottomText(note){
-    let addtext = document.createTextNode(note);
-    div1.appendChild(b); 
-    b.appendChild(addtext)
-      
-}
-div1.addEventListener('click', function(e){
-    let touch = e.target.parentElement;
-    touch.remove();
-    new div1;
-})
-
-
-
-
